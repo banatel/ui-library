89e502448f2268d88b18f117c218141af95af5fc
fsanchez1397 <felipesanchez10@protonmail.com>
2025-10-26 02:13:57 -0400

    fix: 🐛 removed duplicate button and header

--- lib/components/header/index.tsx
+++ lib/components/header/index.tsx
@@ -1,23 +1,56 @@
-import type { HTMLAttributes, ReactNode } from 'react';
+import React from 'react';
+
+import { ButtonRoot } from '../button';
 import './header.css';
 
-export interface HeaderProps extends HTMLAttributes<HTMLElement> {
-  left?: ReactNode;
-  center?: ReactNode;
-  right?: ReactNode;
-  sticky?: boolean;
+type User = {
+  name: string;
+};
+
+export interface HeaderProps {
+  user?: User;
+  onLogin?: () => void;
+  onLogout?: () => void;
+  onCreateAccount?: () => void;
 }
 
-function cx(...parts: Array<string | false | null | undefined>) {
-  return parts.filter(Boolean).join(' ');
-}
-
-export default function Header({ left, center, right, sticky, className, ...rest }: HeaderProps) {
-  return (
-    <header className={cx('header', sticky && 'header--sticky', className)} {...rest}>
-      <div className="header__section header__section--left">{left}</div>
-      <div className="header__section header__section--center">{center}</div>
-      <div className="header__section header__section--right">{right}</div>
-    </header>
-  );
-}
+export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps) => (
+  <header>
+    <div className="storybook-header">
+      <div>
+        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+          <g fill="none" fillRule="evenodd">
+            <path
+              d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
+              fill="#FFF"
+            />
+            <path
+              d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
+              fill="#555AB9"
+            />
+            <path
+              d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
+              fill="#91BAF8"
+            />
+          </g>
+        </svg>
+        <h1>Acme</h1>
+      </div>
+      <div>
+        {user ? (
+          <>
+            <span className="welcome">
+              Welcome, <b>{user.name}</b>!
+            </span>
+            <ButtonRoot onClick={onLogout}  />
+          </>
+        ) : (
+          <>
+            <ButtonRoot onClick={onLogin}  />
+            <ButtonRoot  onClick={onCreateAccount}  />
+          </>
+        )}
+      </div>
+    </div>
+  </header>
+);
